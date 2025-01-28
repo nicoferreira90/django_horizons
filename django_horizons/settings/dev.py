17dc24901e2348518cda47108f8a13b94f11925d
nico <nicolasalfonsoferreira@gmail.com>
2025-01-28 20:54:21 -0300

    first commit. homepage banner.

---
+++ django_horizons/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-x5j9(3*tw7ssw&^8bwpw$2y)x(fg)7vwb7$7#i&@^qe(@=bln^"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
