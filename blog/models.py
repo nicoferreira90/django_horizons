5e11cf168e9b06adf5b74426cf7f803ae374b487
nico <nicolasalfonsoferreira@gmail.com>
2025-02-04 20:37:50 -0300

    blog page working more or less fine. needs css tuning and adding images

--- blog/models.py
+++ blog/models.py
@@ -6,10 +6,13 @@ from wagtail.models import Page
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
 
+from datetime import date
+
 
 class BlogPage(Page):
     blog_title = models.CharField(max_length=100)
     body = RichTextField(blank=True)
+    date = models.DateField("Post date", default=date.today)
 
     content_panels = Page.content_panels + [
         FieldPanel("blog_title"),
