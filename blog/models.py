d6d5294b88539855f4237f8c0a1f3cc870d186d1
nico <nicolasalfonsoferreira@gmail.com>
2025-02-04 19:31:00 -0300

    added simple cover image for blog posts

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
+
+
+class BlogPage(Page):
+    blog_title = models.CharField(max_length=100)
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("blog_title"),
+        FieldPanel("body", classname="full"),
+    ]
