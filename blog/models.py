c3c55f044cf483a3a1a5ba721894c5c08f5057dc
nico <nicolasalfonsoferreira@gmail.com>
2025-02-18 20:08:53 -0300

    progress on blog listing page

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,7 @@ from wagtail.admin.panels import FieldPanel
 from wagtail.images.models import Image
 from blog.blocks import RichTextBlock
 from wagtail.images.blocks import ImageBlock
+from wagtail.contrib.routable_page.models import RoutablePageMixin, route
 
 from datetime import date
 
@@ -42,3 +43,24 @@ class BlogPage(Page):
         verbose_name = "Blog Page"
         verbose_name_plural = "Blog Pages"
         ordering = ["-date"]
+
+
+class BlogListingPage(RoutablePageMixin, Page):
+    """Listing Page lists all blog detail pages"""
+
+    template = "blog/blog_listing_page.html"
+
+    max_count_per_parent = 1
+
+    custom_title = models.CharField(
+        max_length=100, blank=False, null=False, help_text="Page title"
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("custom_title"),
+    ]
+
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request, *args, **kwargs)
+        context["posts"] = BlogPage.objects.live().public()
+        return context
