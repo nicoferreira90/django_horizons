f9310bfd0a1d2ed5d10fd76967123dc4afd31cf4
nico <nicolasalfonsoferreira@gmail.com>
2025-02-20 19:55:46 -0300

    improvements to home page

--- home/models.py
+++ home/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 from wagtail.models import Page
 
+from blog.models import BlogPage
+
 
 class HomePage(Page):
     max_count = 1
+
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request, *args, **kwargs)
+        context["posts"] = BlogPage.objects.live().public().order_by("-date")
+        return context
