01e1e00c0e34143470b5359c5fde160baf57ae6c
nico <nicolasalfonsoferreira@gmail.com>
2025-02-21 13:04:46 -0300

    created about page

--- home/models.py
+++ home/models.py
@@ -4,6 +4,12 @@ from wagtail.models import Page
 
 from blog.models import BlogPage
 
+from wagtail.fields import RichTextField, StreamField
+from wagtail.admin.panels import FieldPanel
+from wagtail.images.models import Image
+from blog.blocks import RichTextBlock
+from wagtail.images.blocks import ImageBlock
+
 
 class HomePage(Page):
     max_count = 1
@@ -12,3 +18,20 @@ class HomePage(Page):
         context = super().get_context(request, *args, **kwargs)
         context["posts"] = BlogPage.objects.live().public().order_by("-date")
         return context
+
+
+class AboutPage(Page):
+    max_count = 1
+
+    body = StreamField(
+        [
+            ("image", ImageBlock()),
+            ("full_richtext", RichTextBlock()),
+        ],
+        null=True,
+        blank=True,
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("body", classname="full"),
+    ]
